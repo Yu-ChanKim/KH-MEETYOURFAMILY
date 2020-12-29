09918d8b3c6dd297727ee3729bb0ff2ba06efb62
bubkyu <educobk@naver.com>
2020-12-29 22:07:11 +0900

    12월29일 로그인기능추가

--- src/user/User.java
+++ src/user/User.java
@@ -3,6 +3,56 @@ package user;
 public class User {
 
 	private String userID;
+	private String userPassword;
+	private String userGender;
+	private String userName;
+	private String userDate;
+	private String userChoice;
+	private String userAnswer;
+	
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPassword;
+	}
+	public void setUserPW(String userPW) {
+		this.userPassword = userPW;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserDate() {
+		return userDate;
+	}
+	public void setUserDate(String userDate) {
+		this.userDate = userDate;
+	}
+	public String getUserChoice() {
+		return userChoice;
+	}
+	public void setUserChoice(String userChoice) {
+		this.userChoice = userChoice;
+	}
+	public String getUserAnswer() {
+		return userAnswer;
+	}
+	public void setUserAnswer(String userAnswer) {
+		this.userAnswer = userAnswer;
+	}
 	
 	
 }
