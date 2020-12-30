7ce661507c8ad3cc46b77421cc574ba072497479
rhdiddlsms <rhdiddlsms@naver.com>
2020-12-30 16:31:13 +0900

    이민법 sql 2차수정

--- SQL/userinfo.sql
+++ SQL/userinfo.sql
@@ -1,7 +1,5 @@
-DROP TABLE USERINFO;
-
 CREATE TABLE USERINFO (
-    userID VARCHAR(20) PK_POST_ID PRIMARY KEY,
+    userID VARCHAR(20) PRIMARY KEY,
     userPassword VARCHAR(20) NOT NULL,
     userGender VARCHAR(20) NOT NULL,
     userName VARCHAR(20) NOT NULL,
@@ -10,6 +8,6 @@ CREATE TABLE USERINFO (
     userAnswer VARCHAR(20) NOT NULL
 );
 
-INSERT INTO USERINFO VALUES('gmchan','123','남자', '이금찬', '19961112', '2', '인천');
-INSERT INTO USERINFO VALUES('minjae','456','남자', '김민재', '19960000', '2', '김포');
-INSERT INTO USERINFO VALUES('qjqrb','789','남자', '김법규', '19890000', '2', '구리');
+INSERT INTO USERINFO VALUES('gmchan','123','남자', '이금찬', '19961112', 2, '인천');
+INSERT INTO USERINFO VALUES('minjae','456','남자', '김민재', '19960000', 2, '김포');
+INSERT INTO USERINFO VALUES('qjqrb','789','남자', '김법규', '19890000', 2, '구리');
